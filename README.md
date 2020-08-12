24e70d6b5604a0c786107fb43613f705ec0099a3
zagovorichev <zagovorichev@gmail.com>
2020-08-12 12:07:57 +0300

    - improved installer - added readme docs

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Container that has _AWS Glue_ under the _Apache Maven_ and _Spark_ for developing with *Python* language usage.
 
 ## Installation
+
+1. `git clone https://github.com/zagovorichev/aws-glue-docker.git`
+2. `cd aws-glue-docker`
+3. `docker-compose up -d`
+4. `docker exec -it aws-glue-container /bin/bash /root/bin/install.sh`
--- bin/install.sh
+++ bin/install.sh
@@ -1,28 +1,18 @@
 # "printenv"
-echo "Clean UP"
+echo "===>"
+echo "1. Clean UP"
 rm -rf /root/lib/* > /dev/null 2>&1
 
-echo "1. Run apache maven extractor"
+echo "2. Run apache maven extractor"
 tar xvf /root/src/apache-maven-3.6.0-bin.tar.gz -C /root/lib/ > /dev/null 2>&1
 
-## add to path
-# PATH=/root/lib/apache-maven-3.6.0/bin:$PATH; export PATH ## for the session only
-NEW_PATH=/root/lib/apache-maven-3.6.0/bin:$PATH
-sed -i 's/PATH=.*//g' /etc/environment
-echo "PATH=$NEW_PATH" >> /etc/environment
-PATH=$NEW_PATH
-echo "path with maven: $PATH"
-
 echo ""
-echo "2. Run spark extractor"
+echo "3. Run spark extractor"
 tar xvf /root/src/spark-2.4.3-bin-hadoop2.8.tgz -C /root/lib/ > /dev/null 2>&1
-## add to path
-SPARK_HOME="/root/lib/spark-2.4.3-bin-spark-2.4.3-bin-hadoop2.8"; export SPARK_HOME
-echo "SPARK_HOME=$SPARK_HOME" >> /etc/environment
-echo "path to spark: $SPARK_HOME"
+mv "/root/lib/spark-2.4.3-bin-spark-2.4.3-bin-hadoop2.8" "/root/lib/spark-2.4.3-bin-hadoop2.8"
 
 echo ""
-echo "3. AWS Glue libs"
+echo "4. AWS Glue libs"
 cp -r /root/src/aws-glue-libs /root/lib/ > /dev/null 2>&1
 echo "Glue libs copied to the /root/lib"
 cd /root/lib/aws-glue-libs/ || exit
@@ -30,29 +20,15 @@ git checkout -b glue-1.0 origin/glue-1.0
 
 # make PyGlue.zip and download additional jar for using with maven
 echo ""
-echo "4. Run glue-setup"
+echo "5. Run glue-setup"
 cd /root/lib/aws-glue-libs || exit
 chmod +x /root/lib/aws-glue-libs/bin/glue-setup.sh > /dev/null 2>&1
 /root/lib/aws-glue-libs/bin/glue-setup.sh > /root/lib/installation.log
 
 echo ""
-echo "5. Replace the line that should not be used anymore (with glue-setup.sh)"
+echo "6. Replace the line that should not be used anymore (with glue-setup.sh)"
 sed -i "s/mvn -f \$ROOT_DIR\/pom\.xml/# mvn -f \$ROOT_DIR\/pom\.xml/g" /root/lib/aws-glue-libs/bin/glue-setup.sh
 
 echo ""
-echo "6. Copy correct jar file from the spark to the aws libs"
-cp $SPARK_HOME/jars/netty-all-4.1.17.Final.jar /root/lib/aws-glue-libs/jarsv1/
-
-# Set environment variables
-# The AWS provided scripts that launch Glue have limitations, but under the hood they basically run Spark after setting up particular environment variables. We need to set those manually to run Spark like Glue in our own way.
-echo ""
-echo "7. Set environment variables"
-SPARK_CONF_DIR=/root/lib/aws-glue-libs/conf; export SPARK_CONF_DIR
-echo "SPARK_CONF_DIR=$SPARK_CONF_DIR" >> /etc/environment
-PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.10.7-src.zip:/root/lib/aws-glue-libs/PyGlue.zip:${PYTHONPATH}"; export PYTHONPATH
-echo "PYTHONPATH=$PYTHONPATH" >> /etc/environment
-
-echo "environment variables: "
-echo "================"
-"printenv"
-echo "================"
+echo "7. Copy correct jar file from the spark to the aws libs"
+cp "$SPARK_HOME"/jars/netty-all-4.1.17.Final.jar /root/lib/aws-glue-libs/jarsv1/
